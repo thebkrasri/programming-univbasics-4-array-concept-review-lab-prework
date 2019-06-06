2fcd74f29ce6a00e439cb63669fc66bf768b00be
Adriana Guerra <adriana.luz.guerra@gmail.com>
2019-06-06 20:32:46 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,8 +3,8 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  max = Integer.new
-  array.length.times{|index|
+  max = 0
+  array.length.times { |index|
     max = array[index] if array[index] > max
   }
   return max
